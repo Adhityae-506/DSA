7f1ef75295fa02d848853adc23e34d1fde175ea0
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-09-16 20:43:50 +0530

    Interesting and Enjoyable

---
+++ Leetcode/242. Valid Anagram.java
@@ -0,0 +1,39 @@
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        int[] freq = new int[256];
+
+        for(int i=0;i<s.length();i++)
+        {
+            int ascii1 = (int)s.charAt(i);
+            freq[ascii1]++;  
+        }
+
+        for(int i=0;i<t.length();i++)
+        {
+            int ascii2 = (int)t.charAt(i);
+            freq[ascii2]--;
+        }
+        for(int i=0;i<256;i++)
+        {
+            if(freq[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
