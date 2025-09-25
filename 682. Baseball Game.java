e7995d20ce0be5165636e96c7ca4b4e316eca49d
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-09-25 23:04:31 +0530

    25.09.25 Done

---
+++ 682. Baseball Game.java
@@ -0,0 +1,74 @@
+// You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+// An integer x.
+// Record a new score of x.
+// '+'.
+// Record a new score that is the sum of the previous two scores.
+// 'D'.
+// Record a new score that is the double of the previous score.
+// 'C'.
+// Invalidate the previous score, removing it from the record.
+// Return the sum of all the scores on the record after applying all the operations.
+
+
+// Example 1:
+
+// Input: ops = ["5","2","C","D","+"]
+// Output: 30
+// Explanation:
+// "5" - Add 5 to the record, record is now [5].
+// "2" - Add 2 to the record, record is now [5, 2].
+// "C" - Invalidate and remove the previous score, record is now [5].
+// "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+// "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+// The total sum is 5 + 10 + 15 = 30.
+// Example 2:
+
+// Input: ops = ["5","-2","4","C","D","9","+","+"]
+// Output: 27
+// Explanation:
+// "5" - Add 5 to the record, record is now [5].
+// "-2" - Add -2 to the record, record is now [5, -2].
+// "4" - Add 4 to the record, record is now [5, -2, 4].
+// "C" - Invalidate and remove the previous score, record is now [5, -2].
+// "D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+// "9" - Add 9 to the record, record is now [5, -2, -4, 9].
+// "+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+// "+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+// The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+
+
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i = 0;i<operations.length;i++){
+            if(operations[i].equals("C")){
+                stack.pop();
+            }
+            else if(operations[i].equals("D")){
+                stack.push(stack.peek() * 2);
+            }
+            else if(operations[i].equals("+")){
+                int last = stack.peek();
+                int secondLast = stack.get(stack.size() - 2);
+                stack.push(last + secondLast);
+            }else{
+                stack.push(Integer.parseInt(operations[i]));
+            }
+        }
+        int sum = 0;
+        while(!stack.isEmpty()){
+            sum+= stack.pop();
+        }
+
+        return sum;
+           
+    }
+}
+/*
+1) Create a Stack to perform the opertion listed down there.
+2) if operation[i] == C and pop the operations[i - 1]
+
+*/
