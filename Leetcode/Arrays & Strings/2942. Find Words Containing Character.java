bae95918de4fdec1582a5e759690c43fb7d3c91d
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-10-15 14:11:14 +0530

    Day-31 15.10.25

---
+++ Leetcode/Arrays & Strings/2942. Find Words Containing Character.java
@@ -0,0 +1,33 @@
+// Example 1:
+
+// Input: words = ["leet","code"], x = "e"
+// Output: [0,1]
+// Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+// Example 2:
+
+// Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+// Output: [0,2]
+// Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+// Example 3:
+
+// Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+// Output: []
+
+// Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for(int i=0;i<words.length;i++){
+            String pivot = words[i];
+            for(int j=0;j<pivot.length();j++){
+                if(pivot.charAt(j)==x){
+                    numbers.add(i);
+                    break;
+                }
+            }
+        }
+        return numbers;
+    }
+}
