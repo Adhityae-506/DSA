52027573c2f4135b1a6f7ef6ef8c48de728584c5
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-10-22 13:14:45 +0530

    Day-38 22.10.25

---
+++ Leetcode/Arrays & Strings/14. Longest Common Prefix.java
@@ -0,0 +1,27 @@
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int minLength = Math.min(first.length(), 
+                                 	last.length());
+        
+      	int i = 0;
+        while (i < minLength && first.charAt(i) == last.charAt(i)) {
+            i++;
+        }
+
+        return first.substring(0, i);
+    }
+}
