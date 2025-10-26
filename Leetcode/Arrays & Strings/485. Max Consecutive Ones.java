4a64e93a1ee2ff989fbf8363ccde138481005620
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-10-26 19:14:55 +0530

    Day-41 25.10.25

---
+++ Leetcode/Arrays & Strings/485. Max Consecutive Ones.java
@@ -0,0 +1,28 @@
+// Example 1:
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+// Example 2:
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max_count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                if(max_count < count){
+                    max_count = count;
+                }
+            }else{
+                count = 0;
+            }
+        }
+        return max_count;
+        
+    }
+}
