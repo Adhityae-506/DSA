2ca113c16af4286cf8a339dcafba01620b654c98
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-10-31 13:50:57 +0530

    Day-47 31.10.25

---
+++ Leetcode/Arrays & Strings/3289. The Two Sneaky Numbers of Digitville.java
@@ -0,0 +1,40 @@
+// Example 1:
+
+// Input: nums = [0,1,1,0]
+
+// Output: [0,1]
+
+// Explanation:
+
+// The numbers 0 and 1 each appear twice in the array.
+
+// Example 2:
+
+// Input: nums = [0,3,2,1,3,2]
+
+// Output: [2,3]
+
+// Explanation:
+
+// The numbers 2 and 3 each appear twice in the array.
+
+// Example 3:
+
+// Input: nums = [7,1,5,4,3,4,6,0,9,5,8,2]
+
+// Output: [4,5]
+
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int[] result = new int[2];
+        int k = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    result[k++] = nums[i];
+                }
+            }
+        }
+        return result;
+    }
+}
