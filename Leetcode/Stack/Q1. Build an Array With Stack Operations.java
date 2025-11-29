63ca8545a958ffe6c3b30065335523a36b3bdf58
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-11-29 14:24:13 +0530

    Day - 76 29.11.25

---
+++ Leetcode/Stack/Q1. Build an Array With Stack Operations.java
@@ -0,0 +1,48 @@
+// Example 1:
+
+// Input: target = [1,3], n = 3
+// Output: ["Push","Push","Pop","Push"]
+// Explanation: Initially the stack s is empty. The last element is the top of the stack.
+// Read 1 from the stream and push it to the stack. s = [1].
+// Read 2 from the stream and push it to the stack. s = [1,2].
+// Pop the integer on the top of the stack. s = [1].
+// Read 3 from the stream and push it to the stack. s = [1,3].
+// Example 2:
+
+// Input: target = [1,2,3], n = 3
+// Output: ["Push","Push","Push"]
+// Explanation: Initially the stack s is empty. The last element is the top of the stack.
+// Read 1 from the stream and push it to the stack. s = [1].
+// Read 2 from the stream and push it to the stack. s = [1,2].
+// Read 3 from the stream and push it to the stack. s = [1,2,3].
+// Example 3:
+
+// Input: target = [1,2], n = 4
+// Output: ["Push","Push"]
+// Explanation: Initially the stack s is empty. The last element is the top of the stack.
+// Read 1 from the stream and push it to the stack. s = [1].
+// Read 2 from the stream and push it to the stack. s = [1,2].
+// Since the stack (from the bottom to the top) is equal to target, we stop the stack operations.
+// The answers that read integer 3 from the stream are not accepted.
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        
+        List<String> result = new ArrayList<>();
+        int curr = 1;
+
+        for(int num : target)
+        {
+            while(curr < num)
+            {
+                result.add("Push");
+                result.add("Pop");
+                curr++;
+            }
+            result.add("Push");
+            curr++;
+        } 
+        return result;
+
+    }
+}
